fa372c396f14d6e406725b1484d0a6508fa01a7f
Jennifer McKee <jennifermckee1212@gmail.com>
2020-04-08 13:40:32 -0400

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -3,7 +3,6 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
-const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
 
@@ -20,7 +19,7 @@ function RenderCampsite({ campsite }) {
     )
 }
 
-function RenderComments({ comments }) {
+function RenderComments({comments, addComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -33,7 +32,7 @@ function RenderComments({ comments }) {
                     </div>
                 )
                 }
-                < CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         );
     }
@@ -67,9 +66,9 @@ class CommentForm extends Component {
     }
 
     Submit(values) {
-        console.log("Current state is: " + JSON.stringify(values));
-        alert("Current state is: " + JSON.stringify(values));
-    }
+            this.toggleModal();
+            this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        }
 
     render() {
         return (
@@ -98,7 +97,6 @@ class CommentForm extends Component {
                                     placeholder="Your Name"
                                     className="form-control"
                                     validators={{
-                                        required, 
                                         minLength: minLength(2),
                                         maxLength: maxLength(15)
                                     }}
@@ -109,7 +107,6 @@ class CommentForm extends Component {
                                         show="touched"
                                         component="div"
                                         messages={{
-                                            required: 'Required',
                                             minLength: 'Must be at least 2 characters',
                                             maxLength: 'Must be 15 characters or less'
                                         }}
@@ -151,7 +148,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
